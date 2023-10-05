cc3f4d51ab32a12ca21f5e874642a959ae7df487
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-10-05 17:14:17 +0200

    rename alt to ocr

--- packages/ocr-text-api/src/server.ts
+++ packages/ocr-text-api/src/server.ts
@@ -7,32 +7,25 @@ import {
 import { init } from "@autometrics/exporter-prometheus";
 import { serve } from "@hono/node-server";
 import { logger } from "hono/logger";
-import { handleImageCaptioning, handleImageOcr, handleRoot } from "./handlers";
+import { handleImageCaptioning, handleImageOcr, handleRoot } from "./handlers.js";
 import { Hono } from "hono";
 
 const app = new Hono();
 
 init();
 
-const AltTextSLO: Objective = {
-	name: "AltTextSLO",
+const OcrTextSLO: Objective = {
+	name: "OcrTextSLO",
 	successRate: ObjectivePercentile.P95,
 	latency: [ObjectiveLatency.Ms2500, ObjectivePercentile.P90],
 };
 
 app.get("/", autometrics(handleRoot));
-app.post(
-	"/api/image-captioning",
-	autometrics(
-		{ objective: AltTextSLO, trackConcurrency: true },
-		handleImageCaptioning,
-	),
-);
 
 app.post(
 	"/api/image-ocr",
 	autometrics(
-		{ objective: AltTextSLO, trackConcurrency: true },
+		{ objective: OcrTextSLO, trackConcurrency: true },
 		handleImageOcr,
 	),
 );
