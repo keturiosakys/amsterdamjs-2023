99b2babcaed6936eda0af1b1fbaccc8f7d7a1a8b
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-10-06 12:58:40 +0200

    explicitly import instrumentation

--- packages/ocr-text-api/src/instrumentation.ts
+++ packages/ocr-text-api/src/instrumentation.ts
@@ -3,7 +3,7 @@ import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
 
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 const sdk = new NodeSDK({
 	traceExporter: new OTLPTraceExporter({
@@ -13,3 +13,4 @@ const sdk = new NodeSDK({
 });
 
 sdk.start();
+console.log("Tracing initialized");
