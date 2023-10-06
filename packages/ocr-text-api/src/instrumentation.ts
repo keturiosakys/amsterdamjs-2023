e4ec7a0bc3b33f2c9485f1f50bb086e49811804e
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-10-06 13:35:43 +0200

    fix up endpoints

--- packages/ocr-text-api/src/instrumentation.ts
+++ packages/ocr-text-api/src/instrumentation.ts
@@ -1,13 +1,22 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
 
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
 
+const exportEndpoint = "http://mutual-chickens.railway.internal:4318";
+
 const sdk = new NodeSDK({
+	metricReader: new PeriodicExportingMetricReader({
+		exporter: new OTLPMetricExporter({
+			url: `${exportEndpoint}/v1/metrics`,
+		}),
+	}),
 	traceExporter: new OTLPTraceExporter({
-		url: "http://mutual-chickens.railway.internal:4317/v1/traces",
+		url: `${exportEndpoint}/v1/traces`,
 	}),
 	instrumentations: [getNodeAutoInstrumentations()],
 });
